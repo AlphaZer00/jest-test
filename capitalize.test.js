aede637189b813bd5bd9d77f53faadbd94b299b5
AlphaZer00 <batudasdemir@gmail.com>
2024-09-01 11:20:14 -0400

    Create file

--- capitalize.test.js
+++ capitalize.test.js
@@ -1,5 +1,5 @@
-import capitalize from './capitalize.js'
+import { capitalize } from "./capitalize.js";
 
-test('Returns with first character capitalized', () => {
-    expect(capitalize('string')).toBe('String');
+test("Returns with first character capitalized", () => {
+	expect(capitalize("string")).toBe("String");
 });
